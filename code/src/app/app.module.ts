09cd56f0977ac1bc6ef4a79c83db906edcc323c3
jekasrs <jekasjeny1012@gmail.com>
2023-10-21 23:56:50 +0200

    Привел хоть к какому-то чиаему виду проект.

--- code/src/app/app.module.ts
+++ code/src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
 
 import { AppComponent } from './app.component';
-import {HttpClientModule} from "@angular/common/http";
+import {CardItemComponent} from "./product-page/card-item/card-item.component";
+import {FilterComponent} from "./filter/filter.component";
+import {HeaderComponent} from "./header/header.component";
+import { MainPageComponent } from './main-page/main-page.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
+import { UserPageComponent } from './user-page/user-page.component';
+import { PharmacistPageComponent } from './pharmacist-page/pharmacist-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, CardItemComponent, FilterComponent,
+    HeaderComponent, MainPageComponent, CartPageComponent,
+    LoginPageComponent, RegistrationPageComponent, UserPageComponent,
+    PharmacistPageComponent
   ],
   imports: [
     BrowserModule,
--- code/src/app/filter/filter.component.ts
+++ code/src/app/filter/filter.component.ts
@@ -1,5 +1,4 @@
 import {Component} from "@angular/core";
-import {filter} from "rxjs";
 
 @Component({
   selector: 'app-filter',
