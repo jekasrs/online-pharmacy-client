8a9d27297ab263f115e3bba240de62aaeeabe1ee
maryshekk <mashastik2013@mail.ru>
2023-11-02 00:44:01 +0300

    изменила фильтр-кнопки, создала model

--- code/src/app/components/filter/filter.component.ts
+++ code/src/app/components/filter/filter.component.ts
@@ -1,5 +1,7 @@
-import {Component} from "@angular/core";
+import {Component, Input} from "@angular/core";
 import {filter} from "rxjs";
+import {IFilter} from "../../models/filter";
+import {IUser} from "../../models/user";
 
 @Component({
   selector: 'app-filter',
@@ -8,5 +10,6 @@ import {filter} from "rxjs";
 })
 export class FilterComponent
 {
+  @Input() user: IUser
   title = 'Filter'
 }
