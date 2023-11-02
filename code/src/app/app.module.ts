82f2fd553945539732e1cd977cdf8391c6d691cb
maryshekk <mashastik2013@mail.ru>
2023-11-02 03:18:39 +0300

    реализована плашка заказа в корзине

--- code/src/app/app.module.ts
+++ code/src/app/app.module.ts
@@ -3,24 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
 
 import { AppComponent } from './app.component';
-import {ProductCardComponent} from "./components/product-card/product-card.component";
-import {FilterComponent} from "./components/filter/filter.component";
+import { ProductCardComponent } from "./components/product-card/product-card.component";
+import { FilterComponent } from "./components/filter/filter.component";
 import {HeaderComponent} from "./components/header/header.component";
 import { MainPageComponent } from './components/main-page/main-page.component';
-import { CartPageComponent } from './components/cart-page/cart-page.component';
+import { CartComponent } from './components/cart-page/cart.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { RegistrationPageComponent } from './components/registration-page/registration-page.component';
 import { UserPageComponent } from './components/user-page/user-page.component';
 import { PharmacistPageComponent } from './components/pharmacist-page/pharmacist-page.component';
-import {ItemComponent} from "./components/main-page/item/item.component";
+import { ItemComponent } from "./components/main-page/item/item.component";
+import { CartItemCardComponent } from "./components/cart-page/cart-item-card/cart-item-card.component";
+import { CartOrderCardComponent } from "./components/cart-page/cart-order-card/cart-order-card.component";
 
 @NgModule({
   declarations: [
     AppComponent, ProductCardComponent, FilterComponent,
-    HeaderComponent, MainPageComponent, CartPageComponent,
+    HeaderComponent, MainPageComponent, CartComponent,
     LoginPageComponent, RegistrationPageComponent, UserPageComponent,
     PharmacistPageComponent,ItemComponent,
-    ProductCardComponent
+    ProductCardComponent, CartItemCardComponent, CartOrderCardComponent
   ],
   imports: [
     BrowserModule,
