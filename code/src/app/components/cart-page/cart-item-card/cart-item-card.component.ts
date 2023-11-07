ccc6d0fd5094f7fc20c677defbf6a093785ac73f
maryshekk <mashastik2013@mail.ru>
2023-11-07 22:42:33 +0300

    added increase and deacrease functions for items quantity

--- code/src/app/components/cart-page/cart-item-card/cart-item-card.component.ts
+++ code/src/app/components/cart-page/cart-item-card/cart-item-card.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class CartItemCardComponent {
   title: 'cart-item-card'
+  quantity: number = 1
+
+  increaseQuantity()
+  {
+    this.quantity++;
+  }
+
+  decreaseQuantity()
+  {
+    if (this.quantity != 1)
+    {
+      this.quantity--;
+    }
+  }
 }
