79ff86735481998feb87ddd18b2d3834450855fe
maryshekk <mashastik2013@mail.ru>
2023-11-08 18:57:16 +0300

    dynamic display of order info in cart. added createOrder() function.

--- code/src/app/components/cart-page/cart.component.ts
+++ code/src/app/components/cart-page/cart.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {items} from "../../data/items";
+import {orders} from "../../data/orders";
+import {IOrder} from "../../models/order";
+import {IItem} from "../../models/item";
+import {IItemQuantuty} from "../../models/item_quantity";
 
 @Component({
   selector: 'app-cart',
@@ -7,5 +11,22 @@ import {items} from "../../data/items";
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
-  protected readonly items = items;
+  @Input() order: IOrder
+  // protected readonly items = items;
+  // items: IItemQuantuty[];
+
+  // предполагается, что items будет инициализироваться
+  // по соответствующему order
+  // protected readonly items = items;
+
+  // constructor() {
+  //   this.items = this.order.items;
+  //   for (let i = 0; i < this.order.items.length; i++)
+  //   {
+  //     this.items[i] = items[this.order.items[0].itemId]
+  //     // находим в бд соответствующий item по его id
+  //     // нужно будет модифицировать для случая,
+  //     // когда порядок item в бд не соответствует его id
+  //   }
+  // }
 }
--- code/src/app/data/orders.ts
+++ code/src/app/data/orders.ts
@@ -4,17 +4,19 @@ export const orders: IOrder[] = [
   {
     id: 0,
     date: '13.10.23',
-    adress: 'Аптека Вита, ул. Ленинская 310',
+    address: 'Аптека Вита, ул. Ленинская 310',
     deliverDate: '15.10.23',
     price: '1000',
-    orderNumber: '00000001'
+    orderNumber: '00000001',
+    items: [{itemId: 0, itemQuantity: 1}, {itemId: 1, itemQuantity: 2}]
   },
   {
     id: 1,
     date: '10.09.23',
-    adress: 'Аптека Вита, ул. Комсомольская 30',
+    address: 'Аптека Вита, ул. Комсомольская 30',
     deliverDate: '15.10.23',
     price: '1000.50',
-    orderNumber: '00000002'
+    orderNumber: '00000002',
+    items: [{itemId: 1, itemQuantity: 2}]
   }
 ]
