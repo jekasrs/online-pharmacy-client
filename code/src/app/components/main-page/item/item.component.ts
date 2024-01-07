4b7ef0e9a7e5752af5ac89cc93a6dfa4a1adb441
maria.ivanova <maria.ivanova@vk.team>
2024-01-07 22:59:47 +0300

    checked font, added navigation

--- code/src/app/components/main-page/item/item.component.ts
+++ code/src/app/components/main-page/item/item.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, Output} from "@angular/core";
 import {IItem} from "../../../models/item";
 import {IItemQuantuty} from "../../../models/item_quantity";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-item',
@@ -14,6 +15,10 @@ export class ItemComponent {
   quantityIsZero = true
   quantity = 0
   @Output() itemQuantity: IItemQuantuty
+
+  constructor(private router: Router) {
+  }
+
 
   increaseQuantity() {
     this.quantity++;
@@ -45,4 +50,8 @@ export class ItemComponent {
   updateItemQuantity() {
     this.itemQuantity.itemQuantity = this.quantity
     }
+
+  onProductCardPage() {
+    this.router.navigate(["./product-page", {itemData: this.itemQuantity}])
+  }
 }
